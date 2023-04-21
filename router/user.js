881cb75d450ead4ba16d09519fd987f06f47fa33
Tarpan Bhattacharya <67609534+phantom-kudo@users.noreply.github.com>
2023-04-21 16:52:38 +0530

    Blog api (#3)
    * created blogController
    
    * delete blog fixed
    
    * getUserDetails Api

--- router/user.js
+++ router/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = express.Router();
-const { signUp, signIn } = require("../controller/user");
+const { signUp, signIn, getUserDetails } = require("../controller/user");
+const { auth } = require("../middleware/auth");
 
 userRouter.post("/sign-up", signUp);
 userRouter.post("/sign-in", signIn);
+userRouter.get("/get-user-details", auth, getUserDetails);
 
 module.exports = userRouter;
